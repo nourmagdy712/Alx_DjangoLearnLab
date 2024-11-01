499197754636719e026f059e183ba84241aaa01d
Nour Magdy <nourmagdy574@gmail.com>
2024-11-01 13:22:03 +0200

    adding admin

--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+from django.http import HttpResponse
+def index(request): return HttpResponse("Welcome to my book store.”)
+
+#In urls.py:
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("books/", include("book_store.urls")),
+    path("admin/", admin.site.urls),
+]
+"""
