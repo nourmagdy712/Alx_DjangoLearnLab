dcfc0de878ab3430301f1f3c5260d545efbf6437
Nour Magdy <nourmagdy574@gmail.com>
2024-11-16 18:08:37 +0200

    thirddd

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,15 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
+
+class ExampleForm(forms.Form):
+    # Define your form fields here
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea)
+
+    # You can also add custom validation or widgets
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if not title.isalpha():
+            raise forms.ValidationError("Title should only contain letters")
+        return title
