232de6248efed12e3de96f60c8186d9b268188d9
Nour Magdy <nourmagdy574@gmail.com>
2024-11-16 08:18:43 +0200

    add this

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import BaseUserManager
-
+from relationship_app.models import CustomUser  # Update this import to your app's custom user model
 
 # Author Model
 class Author(models.Model):
@@ -36,41 +32,9 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
     
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
-        """
-        Create and return a regular user with an email and password.
-        """
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        """
-        Create and return a superuser with an email and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(username, email, password, **extra_fields)    
-    
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    objects = CustomUserManager()  # Assign the custom manager
-
-
-    def __str__(self):
-        return self.username
+class Profile(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    bio = models.TextField()
+    location = models.CharField(max_length=100)    
     
 
-    def clean(self):
-        # Ensure the user is not younger than 13 years old
-        if self.date_of_birth:
-            age = timezone.now().year - self.date_of_birth.year
-            if age < 13:
-                raise ValidationError("User must be at least 13 years old.")
