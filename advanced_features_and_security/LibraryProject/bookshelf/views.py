9763644924b72231f5a54d3d6f16a44cad0c2a17
Nour Magdy <nourmagdy574@gmail.com>
2024-11-16 18:09:20 +0200

    third

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # Create your views here.
 
