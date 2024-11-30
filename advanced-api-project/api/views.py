287a57a4b09fecff16e62f270130c3b7bdbbef88
Nour Magdy <nourmagdy574@gmail.com>
2024-11-30 18:48:45 +0200

    three

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters import rest_framework
 from rest_framework import generics
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
