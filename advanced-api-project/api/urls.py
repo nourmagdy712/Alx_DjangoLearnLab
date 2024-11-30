d6be4199667ffa9cdaef5e78e4c23893f74087de
Nour Magdy <nourmagdy574@gmail.com>
2024-11-30 18:42:36 +0200

    two

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),  # List all books and create a new book
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # Retrieve, update, or delete a single book by ID
     path('books/create/', BookCreateView.as_view(), name='book-create'),  # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update an existing book by ID
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book by ID
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),  # Update an existing book by ID
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book by ID
 ]
