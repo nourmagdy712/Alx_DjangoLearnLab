3f5bdfd93091586984e1eebb3b1b5e8c0ae4feb8
Nour Magdy <nourmagdy574@gmail.com>
2024-11-30 18:45:12 +0200

    second

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 import django_filters
 
 # List View: Retrieve all books
