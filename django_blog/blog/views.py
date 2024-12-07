2932b832e3dee1ef3e389205b94ccdbe16f13af0
Nour Magdy <nourmagdy574@gmail.com>
2024-12-07 13:00:16 +0200

    second

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user immediately after registration
+            return redirect('profile')  # Redirect to the profile page
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')  # Redirect to the profile page after update
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'registration/profile.html', {'form': form})
