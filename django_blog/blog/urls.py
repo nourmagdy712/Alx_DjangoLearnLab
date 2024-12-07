d414736ba0ca7b97757cd27938c3819254a48bec
Nour Magdy <nourmagdy574@gmail.com>
2024-12-07 14:21:43 +0200

    third

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+
 ]
