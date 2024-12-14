10d822b6a23794d4d979f13c8f156263f5057723
Nour Magdy <nourmagdy574@gmail.com>
2024-12-14 14:10:06 +0200

    three

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -7,7 +7,12 @@ from django.contrib.auth import get_user_model
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-   
+    following = models.ManyToManyField(
+        'self', 
+        related_name='followers',  # This will give you reverse access from the other side
+        symmetrical=False,  # Users can follow other users without mutual following
+        blank=True
+    )
     def __str__(self):
         return self.username
     
