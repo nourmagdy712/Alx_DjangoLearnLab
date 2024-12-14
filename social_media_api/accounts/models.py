731bb658c1f74356ca38fe627520bf4fd2c246e4
Nour Magdy <nourmagdy574@gmail.com>
2024-12-14 14:10:49 +0200

    thirdtask

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -16,12 +16,3 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
     
-class Follower(models.Model):
-    follower = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
-    following = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('follower', 'following')
-
-    def __str__(self):
-        return f"{self.follower.username} follows {self.following.username}"
